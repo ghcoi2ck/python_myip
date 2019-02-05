4ec113bb207140c30a1b160aa363c660ae1f4e54
Jordan <asdfjklasjkldf@gmail.com>
2019-02-05 05:34:36 +0000

    new procfile among other things

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+application = Flask(__name__)
+
+@application.route('/')
+def myip():
+  return request.remote_addr
+
+if __name__ == '__main__':
+  application.run(host='0.0.0.0')
